83f2d1055b8d66d075cb5e3996cde2b8dd33a335
vimuths123 <vimuths@yahoo.com>
2016-02-07 02:43:44 +0530

    New contact changes

--- contact.php
+++ contact.php
@@ -13,32 +13,44 @@ $dbname = "salunz";
 $conn = new mysqli($hostname, $username, $password, $dbname);
 
 if (isset($_POST['email'])) {
-    
+
     if ($_POST['time'] == "") {
         echo "You'r time field was empty or notvalid. Refresh again to see the form.";
     } else {
-         $result = $conn->query("SELECT * FROM `booking`");
-         while ($row = $result->fetch_assoc()) {
-             if($row{'booking_time'} == $_POST['time']){
-                 echo "Sorry, You'r time has tacken by someone else. Refresh again to see the form.";
-                 exit();
-             }
-         }
-         
-         $conn->query("INSERT INTO `booking` (`name`,`email`,`booking_time`,`comment`) VALUES ('".$_POST['time']."','".$_POST['time']."','".$_POST['time']."')");
-         
+        $result = $conn->query("SELECT * 
+             FROM `booking` 
+             WHERE `booking_time` = '" . substr($_POST['time'], 11) . "'
+             AND `booking_date` = '" . substr($_POST['time'], 0, 10) . "'");
+
+        if ($result->num_rows > 0) {
+            echo "Sorry, You'r time has tacken by someone else. Refresh again to see the form.";
+            exit();
+        }
+//         while ($row = $result->fetch_assoc()) {
+//             if($row{'booking_time'} == $_POST['time']){
+//                 echo "Sorry, You'r time has tacken by someone else. Refresh again to see the form.";
+//                 exit();
+//             }
+//         }
+//        echo getenv('HTTP_X_FORWARDED_FOR'); //ip
+//        echo substr($_POST['time'],11); //time
+//        echo substr($_POST['time'],0,10); //date
+//        exit();
+
+
+        $conn->query("INSERT INTO `booking` (`name`,`email`,`phone`,`booking_time`,`comment`,`booking_date`,`user_ip`) VALUES ('" . $_POST['name'] . "','" . $_POST['email'] . "','" . $_POST['phone'] . "','" . substr($_POST['time'], 11) . "','" . $_POST['comment'] . "','" . substr($_POST['time'], 0, 10) . "','" . $_SERVER['REMOTE_ADDR'] . "')");
     }
 
-	$name =$_POST["name"];
-	$from =$_POST["email"];
-	$comment=$_POST["comment"];
-	$time = $_POST['time'];
+    $name = $_POST["name"];
+    $from = $_POST["email"];
+    $comment = $_POST["comment"];
+    $time = $_POST['time'];
     $phone = $_POST['phone'];
-	// Email Receiver Address
-	$receiver="db.thushara@gmail.com";
-	$subject="New appoinment for new look";
+    // Email Receiver Address
+    $receiver = "db.thushara@gmail.com";
+    $subject = "New appoinment for new look";
 
-	$message = "
+    $message = "
 	<html>
 	<head>
 	<title>HTML email</title>
@@ -54,43 +66,40 @@ if (isset($_POST['email'])) {
 	</tr>
 	<tr>
 	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>Name:</td>
-	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$name."</td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>" . $name . "</td>
 	</tr>
 	<tr>
 	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>Email:</td>
-	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$from."</td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>" . $from . "</td>
 	</tr>
 	<tr>
 	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>Phone:</td>
-	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$phone."</td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>" . $phone . "</td>
 	</tr>
 	<tr>
     <tr>
     <td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>Time of appoinment:</td>
-    <td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$time."</td>
+    <td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>" . $time . "</td>
     </tr>
     <tr>
 	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>Message:</td>
-	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".nl2br($comment)."</td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>" . nl2br($comment) . "</td>
 	</tr>
 	</table>
 	</body>
 	</html>
 	";
 // Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 // More headers
-$headers .= 'From: <'.$from.'>' . "\r\n";
-  if(mail($receiver,$subject,$message,$headers))  
-  {
-	   //Success Message
-     echo 'Time you entered has successfully sent to confirm. Thanks for visiting';
-  }
-  else
-  {	
-  	 //Fail Message
-     echo "The message could not been sent!";
-  }
+    $headers .= 'From: <' . $from . '>' . "\r\n";
+    if (mail($receiver, $subject, $message, $headers)) {
+        //Success Message
+        echo 'Time you entered has successfully sent to confirm. Thanks for visiting';
+    } else {
+        //Fail Message
+        echo "The message could not been sent!";
+    }
 }
 ?>
